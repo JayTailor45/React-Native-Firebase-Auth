81491a22717f2efcb9d5da456e19394630291f94
Jay Tailor <tailorj64@gmail.com>
2019-05-20 10:17:03 +0530

    Implemented login registratin logic

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import { Card, CardSection, Button, Input } from './common'
+import Firebase from 'firebase';
+import { Text, StyleSheet } from 'react-native'
+import { Card, CardSection, Button, Input } from './common';
 
 class LoginForm extends Component{
 
   state = {
     email: '',
-    password: ''
+    password: '',
+    error: ''
+  }
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    this.setState({error: ''})
+
+    Firebase.auth().signInWithEmailAndPassword(email,password)
+      .catch(() => {
+        Firebase.auth().createUserWithEmailAndPassword(email,password)
+          .catch(() => {
+            this.setState({error: 'Authentication failed.'});
+          })
+      })
   }
 
   render(){
@@ -28,13 +45,28 @@ class LoginForm extends Component{
             isPassword   = {true}
           />
         </CardSection>
+
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button>Log In</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
+            Log In
+          </Button>
         </CardSection>
 
       </Card>
     )
   }
 }
+
+const styles = StyleSheet.create({
+    errorTextStyle: {
+      fontSize: 20,
+      alignSelf: 'center',
+      color: 'red'
+    }
+});
 
 export default LoginForm;
