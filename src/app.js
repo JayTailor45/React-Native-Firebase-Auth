718052ebfb49320c693e90e6d337130d7e459da1
Jay Tailor <tailorj64@gmail.com>
2019-05-20 09:06:26 +0530

    Added reusable components and Initilized firebase

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import Firebase from 'firebase';
+import { Header } from './components/common';
+
+class App extends Component {
+  componentWillMount() {
+    Firebase.initializeApp({
+      apiKey: 'AIzaSyDaZAILuie6aEotLUera0MC5s98ksvWBn4',
+      authDomain: 'rn-auth-5c0ff.firebaseapp.com',
+      databaseURL: 'https://rn-auth-5c0ff.firebaseio.com',
+      projectId: 'rn-auth-5c0ff',
+      storageBucket: 'rn-auth-5c0ff.appspot.com',
+      messagingSenderId: '920760473277',
+      appId: '1:920760473277:web:6664a109220cd576'
+    });
+  }
+
+  render() {
+    return (
+      <View>
+        <Header headerText='Authentication' />
+        <Text>App</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
